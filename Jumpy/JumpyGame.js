3199a205dd88ec0a672771891cece1e6d612a738
Mukilan Karthikeyan <47691192+MukilanKarthikeyan@users.noreply.github.com>
2023-06-12 22:01:18 -0400

    updated the jumpy game to add a end screen

--- Jumpy/JumpyGame.js
+++ Jumpy/JumpyGame.js
@@ -1,37 +1,118 @@
 var scl = 20;
 var w = 800;
 var h = 600;
+let buffer = 100; 
 let block;
-let pillars = [];
+let pillars;
+let counter = 0;
 
+let gameOver = false;
+
+function preload() {
+  //myFont = loadFont('PressStart2P-Regular.ttf');
+}
 
 function setup() {
   createCanvas(w, h);
-  block = new Blocky();
-  pillars.push(new Pillar());
   
+  
+  button = createButton("Play Again");
+  button.position((width - button.width) / 2, height/2 + 50);
+  button.style("background-color", "green")
+  button.style("font-size", "1em")
+  button.style("color", "white")
+  
+  button.style("padding", "5px")
+  button.style("position", "absolute")
+  
+  button.mousePressed(start);
+  button.hide();
+  
+  start();
 }
 
 function keyPressed() {
-  if (key ==" ") {
+  if (key == " ") {
     block.jump();
   }
 }
 
 function draw() {
-  if (random(1) < 0.0075  ) {
-    console.log("new pillar");
-    pillars.push(new Pillar());
+  
+  if (!gameOver) {
+    if (pillars.length == 0 
+        || (pillars[pillars.length - 1].x < (w - (scl * 2) - buffer) 
+        && random(1) < 0.01)) {
+      pillars.push(new Pillar());
+    }
+  
+    background(0);
+  
+    block.update();
+    block.show();
+    if (block.collision()) {
+      over();
+    }
+  
+    for (let i = 0; i < pillars.length; i++) {
+      
+      
+      if (!pillars[i].past && pillars[i].blockPassed(block)) {
+        pillars[i].past = true;
+        counter++;
+      }
+      
+      if (pillars[i].offscreen()) {
+        pillars.splice(i, 1);
+      } 
+      
+      if (pillars.length > 0) {
+        pillars[i].update();
+        pillars[i].show();
+        
+        if (pillars.length != 0 && pillars[i].collide(block)) {
+          gameOver = true;
+        }
+      }
+      
+    }
+  } else {
+    over();
   }
   
-  background(0);
-  
-  block.update();
-  block.show();
-  
-  for (var i = 0; i < pillars.length; i++) {
-    pillars[i].update();
-    pillars[i].show();
-  }
   
 }
+
+function over() {
+  gameOver = true;
+  fill(100);
+  let rectw = 350;
+  let recth = 200;
+  rect((w - rectw) / 2, (h - recth) / 2, rectw, recth);
+  fill(255);
+  //textFont(myFont);
+  textSize(52);
+  textAlign(CENTER);
+  text("GAME OVER", w/2, h/2 - 25);
+  score = "Score: " + counter;
+  textSize(36);
+  text(score, w/2, h/2 + 25);
+  button.show();
+  
+  
+}
+
+function start() {
+  button.hide();
+  
+  block = new Blocky();
+  pillars = [];
+  pillars.push(new Pillar());
+    
+  
+  
+  console.log(pillars.length);
+  gameOver = false;
+  block = new Blocky();
+}
+
