d8c0ebf028ea9257aec207c9c00af46d8b6f9ad1
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-07-15 20:28:48 -0400

    motion with keyboard controls!

--- js_workshop/IK/IK.js
+++ js_workshop/IK/IK.js
@@ -16,8 +16,10 @@ function setup() {
     cnv = createCanvas(w, h);
     centerCanvas();
     background(255);
+
     relorigin = createVector(w/2, h/2);
     end = createVector(relorigin.x + 100, relorigin.y + 100)
+    vel = createVector(0, 0);
     midpoint = createVector(end.x, end.y).sub(relorigin).div(2).add(relorigin);
 
     relend = createVector(end.x - relorigin.x, end.y - relorigin.y);
@@ -31,27 +33,28 @@ function setup() {
   
 function draw() {
     background(255);
+    update();
 
-
-    stroke(2)
-    fill(0, 50);
-    line(relorigin.x, relorigin.y, end.x, end.y);
+    stroke(2, 50)
+    fill(0, 10);
+    line(relorigin.x, relorigin.y, end.x, end.y); // refernce line
 
     fill(0, 20);
     strokeWeight(2);
-    stroke(20);
+    stroke(20, 50);
     circle(relorigin.x, relorigin.y, legLength*4);
     circle(relorigin.x, relorigin.y, legLength*2);
-    circle(end.x, end.y, legLength*2);
+    circle(end.x, end.y, legLength*2);// circles for range of motion
 
     strokeWeight(1);
+    stroke(20)
     fill(120, 145, 175, 200);
-    circle(end.x, end.y, diameter);
+    circle(end.x, end.y, diameter);// mark end effector
 
     fill(255,0,0);
-    circle(midpoint.x, midpoint.y, 5);
+    circle(midpoint.x, midpoint.y, 5);// midpoint mark
 
-    findAngles();
+    
     l1 = createVector(cos( PI/2 - theta - alpha), -sin(PI/2 - theta - alpha)).mult(legLength);
     l1.add(end);
     fill(0, 20);
@@ -64,18 +67,26 @@ function draw() {
     
 }
 
+
+function update() {
+    end.add(vel);
+    limitRot();
+    midpoint.set(end.x, end.y).sub(relorigin).div(2).add(relorigin);
+    relend.set(end.x - relorigin.x, end.y - relorigin.y);
+    findAngles();
+}
+
+function limitRot() {
+    end.sub(relorigin).limit(legLength * 2).add(relorigin);
+}
+
 function findAngles() {
     len = relorigin.dist(end) / 2;
     theta = acos(len / legLength);
 
     alpha = angleref.angleBetween(relend);
 
-
     return theta;
-}
-
-function limitRot() {
-    end.sub(relorigin).limit(legLength * 2).add(relorigin);
 }
 
 function centerCanvas() {
@@ -97,8 +108,23 @@ function mouseDragged(event) {
     
     end.x = mouseX;
     end.y = mouseY;
-    limitRot();
-    midpoint = createVector(end.x, end.y).sub(relorigin).div(2).add(relorigin);
-    relend = createVector(end.x - relorigin.x, end.y - relorigin.y);
+    update();
 }
+
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        vel.set(0, -1);
+    } else if (keyCode == DOWN_ARROW) {
+        vel.set(0, 1);
+    } else if (keyCode == RIGHT_ARROW) {
+        vel.set(1, 0);
+    } else if (keyCode == LEFT_ARROW) {
+        vel.set(-1, 0);
+    } 
+}
+
+function keyReleased() {
+    vel.set(0, 0);
+}
+
 
---
+++ js_workshop/IK/leg.js
@@ -0,0 +1,7 @@
+class Leg {
+    constructor() {
+        this.endEffector = createVector();
+        this.hipAngle = 0;
+        this.theta = 0; //thigh angle
+    }
+}
